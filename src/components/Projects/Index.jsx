e2091d0daba4e41274e5a82156cec21d35c764c8
MuraliAirody <muraliairody@gmail.com>
2024-04-17 16:39:59 +0530

    view more

--- src/components/Projects/Index.jsx
+++ src/components/Projects/Index.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import {
   Container,
   Wrapper,
@@ -13,86 +13,83 @@ import {
 import ProjectCard from "./ProjectCard";
 import { projects } from "../../data/constant";
 
+// Styled button component
+const ViewMoreButton = styled.button`
+width: 20%;
+text-decoration: none;
+text-align: center;
+background: hsla(271, 100%, 50%, 1);
+background: linear-gradient(
+  225deg,
+  hsla(271, 100%, 50%, 1) 0%,
+  hsla(294, 100%, 50%, 1) 100%
+);
+background: -moz-linear-gradient(
+  225deg,
+  hsla(271, 100%, 50%, 1) 0%,
+  hsla(294, 100%, 50%, 1) 100%
+);
+background: -webkit-linear-gradient(
+  225deg,
+  hsla(271, 100%, 50%, 1) 0%,
+  hsla(294, 100%, 50%, 1) 100%
+);
+padding: 10px 10px;
+margin-top: 5px;
+border-radius: 20px;
+border: none;
+color: ${({ theme }) => theme.text_primary};
+font-size: 15px;
+font-weight: 600;
+`;
+
 const Projects = ({ openModal, setOpenModal }) => {
   const [toggle, setToggle] = useState("all");
+  const [projectsToShow, setProjectsToShow] = useState(3);
+
+  useEffect(() => {
+    // Reset projectsToShow to 3 whenever the toggle changes
+    setProjectsToShow(3);
+  }, [toggle]);
+
+  const handleViewMore = () => {
+    setProjectsToShow((prevCount) => prevCount + 3);
+  };
+
   return (
     <Container id="projects">
       <Wrapper>
         <Title>Projects</Title>
         <Desc>
-          I have worked on a wide range of projects, on web apps. Here are some of my projects.
+          I have worked on a wide range of projects, on web apps. Here are some
+          of my projects.
         </Desc>
         <ToggleButtonGroup>
-          {toggle === "all" ? (
-            <ToggleButton active value="all" onClick={() => setToggle("all")}>
-              All
-            </ToggleButton>
-          ) : (
-            <ToggleButton value="all" onClick={() => setToggle("all")}>
-              All
-            </ToggleButton>
-          )}
-          <Divider />
-          {toggle === "web app" ? (
-            <ToggleButton
-              active
-              value="web app"
-              onClick={() => setToggle("web app")}
-            >
-              WEB APP'S
-            </ToggleButton>
-          ) : (
-            <ToggleButton value="web app" onClick={() => setToggle("web app")}>
-              WEB APP'S
-            </ToggleButton>
-          )}
-          <Divider />
-          {toggle === "android app" ? (
-            <ToggleButton
-              active
-              value="android app"
-              onClick={() => setToggle("android app")}
-            >
-              ANDROID APP'S
-            </ToggleButton>
-          ) : (
-            <ToggleButton
-              value="android app"
-              onClick={() => setToggle("android app")}
-            >
-              ANDROID APP'S
-            </ToggleButton>
-          )}
-          <Divider />
-          {toggle === "machine learning" ? (
-            <ToggleButton
-              active
-              value="machine learning"
-              onClick={() => setToggle("machine learning")}
-            >
-              MACHINE LEARNING
-            </ToggleButton>
-          ) : (
-            <ToggleButton
-              value="machine learning"
-              onClick={() => setToggle("machine learning")}
-            >
-              MACHINE LEARNING
-            </ToggleButton>
-          )}
+          {toggleOptions.map((option) => (
+            <React.Fragment key={option.value}>
+              {toggle === option.value ? (
+                <ToggleButton
+                  active
+                  value={option.value}
+                  onClick={() => setToggle(option.value)}
+                >
+                  {option.label}
+                </ToggleButton>
+              ) : (
+                <ToggleButton
+                  value={option.value}
+                  onClick={() => setToggle(option.value)}
+                >
+                  {option.label}
+                </ToggleButton>
+              )}
+              <Divider />
+            </React.Fragment>
+          ))}
         </ToggleButtonGroup>
         <CardContainer>
-          {toggle === "all" &&
-            projects.map((project) => (
-              <ProjectCard
-                key={project.id}
-                project={project}
-                openModal={openModal}
-                setOpenModal={setOpenModal}
-              />
-            ))}
-          {projects
-            .filter((item) => item.category == toggle)
+          {(toggle === "all" ? projects : projects.filter(item => item.category === toggle))
+            .slice(0, projectsToShow)
             .map((project) => (
               <ProjectCard
                 key={project.id}
@@ -102,9 +99,22 @@ const Projects = ({ openModal, setOpenModal }) => {
               />
             ))}
         </CardContainer>
+        {toggle === "all" && projectsToShow < projects.length && (
+          <ViewMoreButton onClick={handleViewMore}>View More</ViewMoreButton>
+        )}
+        {toggle !== "all" && projectsToShow < projects.filter(item => item.category === toggle).length && (
+          <ViewMoreButton onClick={handleViewMore}>View More</ViewMoreButton>
+        )}
       </Wrapper>
     </Container>
   );
 };
 
 export default Projects;
+
+const toggleOptions = [
+  { label: "All", value: "all" },
+  { label: "Web Apps", value: "web app" },
+  { label: "Android Apps", value: "android app" },
+  { label: "Machine Learning", value: "machine learning" },
+];
